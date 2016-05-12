fbf0851bbf2d802323d5414b1eafeee38eb0344f
vanesa <vanesa@gmx.net>
2016-05-12 11:15:12 +0200

    setup travis ci config file and add a test

---
+++ test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import unittest
+from jenkins_exporter import JenkinsCollector
+
+
+class JenkinsCollectorTestCase(unittest.TestCase):
+    # The build statuses we want to export about.
+    # TODO: add more test cases
+
+    def test_prometheus_metrics(self):
+        exporter = JenkinsCollector('')
+        self.assertFalse(hasattr(exporter, '_prometheus_metrics'))
+
+        exporter._setup_empty_prometheus_metrics()
+        self.assertTrue(hasattr(exporter, '_prometheus_metrics'))
+        self.assertEqual(sorted(exporter._prometheus_metrics.keys()), sorted(JenkinsCollector.statuses))
+
+
+if __name__ == "__main__":
+    unittest.main()
